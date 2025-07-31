a07d0041dc77952df51881d4e6bfa9381de37482
RandomAOne <annoyinggenz@gmail.com>
2025-07-31 11:55:25 +0530

    Add files via upload

--- QoL.java
+++ QoL.java
@@ -18,7 +18,13 @@ class QoL {
     public static void println(Object s) {
         System.out.println(s); //@FlareX
     }
-    public static void println() {
-        System.out.println();
+    public static double lerp(double t, double a, double b) {
+        return a + t * (b - a);
+    }
+    public static int random(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
+    public static double random(double min, double max) {
+        return ((Math.random() * (max - min)) + min);
     }
 }
